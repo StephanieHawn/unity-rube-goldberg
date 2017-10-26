261251e3f77f1df42b47eac85a58cb7b5788f004
DESKTOP-U15D713\the_t <stephanie.hwn@gmail.com>
2017-10-26 14:03:33 -0700

    New UI instructions, fan script

---
+++ Assets/_scripts/Fan.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+
+    public int air = 30;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.CompareTag("Ball"))
+        {
+            col.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * air);
+        }
+
+    }
+
+    /*
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+        if (rigidbody != null) {
+            rigidbody.AddForce(transform.forward * air);
+        }
+        
+    }
+    */
+}
